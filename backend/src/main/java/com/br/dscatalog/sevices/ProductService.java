6b49eb9f07ba692df4867023fd718932e8ca6ebe
Yury Rodrigues <yury.rodrigues.22@gmail.com>
2022-01-15 02:52:45 -0300

    Inicio de projeto com testes

---
+++ backend/src/main/java/com/br/dscatalog/sevices/ProductService.java
@@ -0,0 +1,94 @@
+package com.br.dscatalog.sevices;
+
+import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.br.dscatalog.dto.CategoryDTO;
+import com.br.dscatalog.dto.ProductDTO;
+import com.br.dscatalog.entities.Category;
+import com.br.dscatalog.entities.Product;
+import com.br.dscatalog.repositories.CategoryRepository;
+import com.br.dscatalog.repositories.ProductRepository;
+import com.br.dscatalog.sevices.exceptions.DatabaseException;
+import com.br.dscatalog.sevices.exceptions.ResourceNotFoundException;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	@Transactional(readOnly = true)
+	public Page<ProductDTO> findAllPaged(Pageable pageable) {
+		Page<Product> list = repository.findAll(pageable);
+		return list.map(x -> new ProductDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		Product entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entidade não encontrada"));
+		return new ProductDTO(entity, entity.getCategories());
+	}
+
+	@Transactional
+	public ProductDTO insert(ProductDTO dto) {
+		Product entity = new Product();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	@Transactional
+	public ProductDTO update(Long id, ProductDTO dto) {
+		try {
+			Product entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+	public void delete(Long id) {
+
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity Violation");
+		}
+
+	}
+
+	private void copyDtoToEntity(ProductDTO dto, Product entity) {
+
+		entity.setName(dto.getName());
+		entity.setDescription(dto.getDescription());
+		entity.setPrice(dto.getPrice());
+		entity.setImgUrl(dto.getImgUrl());
+		entity.setDate(dto.getDate());
+
+		entity.getCategories().clear();
+		for (CategoryDTO catDto : dto.getCategories()) {
+			Category category = categoryRepository.getOne(catDto.getId());
+			entity.getCategories().add(category);
+		}
+
+	}
+
+}
---
+++ backend/src/test/java/com/br/dscatalog/tests/Factory.java
@@ -0,0 +1,22 @@
+package com.br.dscatalog.tests;
+
+import java.time.Instant;
+
+import com.br.dscatalog.dto.ProductDTO;
+import com.br.dscatalog.entities.Category;
+import com.br.dscatalog.entities.Product;
+
+public class Factory {
+	
+	 public static Product createProduct() {
+		 Product product = new Product(1L, "Phone", "Good Phone", 800.0, "https://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
+		 product.getCategories().add(new Category(2L, "Eletronics"));
+		 return product;
+	 }
+	 
+	 public static ProductDTO createProductDTO() {
+		 Product product = createProduct();
+		 return new ProductDTO(product, product.getCategories());
+	 }
+
+}
