552c3bb3a9905fe7eef9803b7eaa2c83e5a97042
Yury Rodrigues <yury.rodrigues.22@gmail.com>
2022-07-09 23:37:09 -0300

    User Crud

---
+++ backend/src/main/java/com/br/dscatalog/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.br.dscatalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.br.dscatalog.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
