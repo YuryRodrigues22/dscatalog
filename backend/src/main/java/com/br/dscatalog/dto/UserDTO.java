8806b1c0b50021a2b01dd69637a7d235bbcecfa0
Yury Rodrigues <yury.rodrigues.22@gmail.com>
2022-07-10 02:01:36 -0300

    Basic beans validation

--- backend/src/main/java/com/br/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/br/dscatalog/dto/UserDTO.java
@@ -4,14 +4,23 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.br.dscatalog.entities.User;
 
 public class UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	
+	@NotBlank(message = "Campo Não pode ser Vazio")
 	private String firstName;
+	
+	@NotBlank(message = "Campo Não pode ser Vazio")
 	private String lastName;
+	
+	@Email(message = "Favor colocar um email válido")
 	private String email;
 
 	Set<RoleDTO> roles = new HashSet<>();
--- backend/src/main/java/com/br/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/br/dscatalog/dto/UserInsertDTO.java
@@ -1,8 +1,11 @@
 package com.br.dscatalog.dto;
 
+import javax.validation.constraints.NotBlank;
+
 public class UserInsertDTO extends UserDTO {
 	private static final long serialVersionUID = 1L;
 
+	@NotBlank(message = "Campo não pode ser vazio")
 	private String password;
 
 	public UserInsertDTO() {
