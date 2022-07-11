31365638ce47357130c5c804097d70c110cafd8e
Yury Rodrigues <yury.rodrigues.22@gmail.com>
2022-07-10 23:02:12 -0300

    UserUpdateValidator

---
+++ backend/src/main/java/com/br/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,10 @@
+package com.br.dscatalog.dto;
+
+import com.br.dscatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+	private static final long serialVersionUID = 1L;
+
+	
+}
--- backend/src/main/java/com/br/dscatalog/services/UserService.java
+++ backend/src/main/java/com/br/dscatalog/services/UserService.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.br.dscatalog.dto.RoleDTO;
 import com.br.dscatalog.dto.UserDTO;
 import com.br.dscatalog.dto.UserInsertDTO;
+import com.br.dscatalog.dto.UserUpdateDTO;
 import com.br.dscatalog.entities.Role;
 import com.br.dscatalog.entities.User;
 import com.br.dscatalog.repositories.RoleRepository;
@@ -58,7 +59,7 @@ public class UserService {
 	}
 
 	@Transactional
-	public UserDTO update(Long id, UserDTO dto) {
+	public UserDTO update(Long id, UserUpdateDTO dto) {
 		try {
 
 			User entity = repository.getOne(id);
