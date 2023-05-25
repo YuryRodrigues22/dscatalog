c6f671893d7e30ff1f0351944038091acedfffdb
Yury Rodrigues <yury.rodrigues.22@gmail.com>
2023-05-25 18:25:08 -0300

    Pagina Catalogo e Rotas

---
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -0,0 +1,14 @@
+import './styles.css'
+
+const ProductPrice = () => {
+    return (
+        <div>
+            <div className="product-price-container">
+                <span>R$</span>
+                <h3>20000.00</h3>
+            </div>
+        </div>
+    );
+};
+
+export default ProductPrice;
