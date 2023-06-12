3fb20d9f7c3c4c51dbd641106f080b8f6151fe17
Yury Rodrigues <yury.rodrigues.22@gmail.com>
2023-06-12 15:16:37 -0300

    Pagination components

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -1,7 +1,9 @@
 import ProductCard from 'components/ProductCard';
+import Pagination from 'components/Pagination';
 import { Link } from 'react-router-dom';
 import { Product } from 'types/product';
 import './styles.css';
+
 
 const Catalog = () => {
 
@@ -51,6 +53,9 @@ const Catalog = () => {
             <ProductCard product={product} />
           </Link>
         </div>
+      </div>
+      <div className="row">
+        <Pagination />
       </div>
     </div>
   );
